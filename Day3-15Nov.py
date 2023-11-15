a55b7f2e0995f82d83b3e989eb52bbfa266e4c95
marz-9 <150155782+marz-9@users.noreply.github.com>
2023-11-15 20:19:15 +0400

    Update Day3-15Nov.py

--- Day3-15Nov.py
+++ Day3-15Nov.py
@@ -2,10 +2,12 @@
 a1=[1,3,5,7]
 a2=[0,2,4,6,8,9]
 a1.extend(a2)
-print("Test Case 1, merged array: ", *sorted(a1))
+print("Test Case 1, merged array:"
+print(*sorted(a1), sep=",")
 
 #Test Case 2
 a3=[1,3,5,7]
 a4=[0,2,4,6,8,9]
 a3.extend(a4)
-print("Test Case 2, merged array: ", *sorted(a3))
+print("Test Case 2, merged array:"
+print(*sorted(a3), sep=",")
