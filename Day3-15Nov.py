7ebb860a5b8329158ff4fa2ebc13fef9f18cd7d1
marz-9 <150155782+marz-9@users.noreply.github.com>
2023-11-15 20:20:09 +0400

    Update Day3-15Nov.py

--- Day3-15Nov.py
+++ Day3-15Nov.py
@@ -6,8 +6,8 @@ print("Test Case 1, merged array:"
 print(*sorted(a1), sep=",")
 
 #Test Case 2
-a3=[1,3,5,7]
-a4=[0,2,4,6,8,9]
+a3=[10,12]
+a4=[5,18,20]
 a3.extend(a4)
 print("Test Case 2, merged array:"
 print(*sorted(a3), sep=",")
