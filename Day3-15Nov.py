8f7d041fa953ec10cd705654eec618f1af36b1c6
marz-9 <almarzooqi.r99@gmail.com>
2023-11-15 19:14:11 +0400

    Problem of the Day

---
+++ Day3-15Nov.py
@@ -0,0 +1,11 @@
+#Test Case 1
+a1=[1,3,5,7]
+a2=[0,2,4,6,8,9]
+a1.extend(a2)
+print("Test Case 1, merged array: ", *sorted(a1))
+
+#Test Case 2
+a3=[1,3,5,7]
+a4=[0,2,4,6,8,9]
+a3.extend(a4)
+print("Test Case 2, merged array: ", *sorted(a3))
